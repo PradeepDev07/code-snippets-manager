f9bcaf0f54f5b2d64f5a634059c3d0cb344924c7
PradeepDev07 <pradeep.m.developer@gmail.com>
2026-01-26 22:22:31 +0530

    DEBUG : Signup button syntex corrected

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -106,9 +106,8 @@ const LoginForm = () => {
             <div>
               <button
                 type="submit"
-                className=`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-200
-              ${isLoading : "bg-blue-300 hover:bg-blue-300"  }`
-                disable = {isLoading ? "true" : "false"}
+                className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${isLoading ? "bg-blue-300 hover:bg-blue-300 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-200`}
+                disabled={isLoading}
               >
                 Sign in
               </button>
